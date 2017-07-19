8cd959af17140a26a75a93b3ee4ad58261312fcf
sumanth <sumanth.724@gmail.com>
2017-07-19 15:27:31 -0700

    changed the steering constraint to 25 degrees

--- src/MPC.h
+++ src/MPC.h
@@ -7,7 +7,7 @@
 using namespace std;
 
 // Scale factors for Cost
-#define STEERING_CONSTRAINT 0.523599f     // 30 degrees in radians
+#define STEERING_CONSTRAINT 0.436332f     // 25 degrees in radians
 #define REFERENCE_VELOCITY 40
 #define REFERENCE_ERROR 0
 #define REFERENCE_CTE 0
